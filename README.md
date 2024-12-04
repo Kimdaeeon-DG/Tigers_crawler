e480a5942c6ca6c824d2bde1fcb48d89fd8e1342
daeekim <asdxl1030@naver.com>
2024-12-04 19:00:34 +0900

    속도 개선 및 회원가입 삭제

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Tigers crawler 구현
 >대구대학교 타이거즈 화면 성적을 음성 혹은 텍스트로 반환해주는 프로그램
 
+[성적 조회 사이트](https://tg.mydu.duckdns.org/)
 
 # 개발환경
 >python,
--- Tools/crawler.py
+++ Tools/crawler.py
@@ -1,45 +1,44 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 import time
 
-def craw(ID, PW, year, semester):
+def craw(id, passwd, year, semester):
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
     options.add_argument('window-size=5000, 5000')
     driver = webdriver.Chrome(options=options)
     driver.set_window_size(5000, 5000)
 
-    time.sleep(1)
 
     URL = "https://sso.daegu.ac.kr/dgusso/ext/tigersstd/login_form.do?Return_Url=https://tigersstd.daegu.ac.kr/nxrun/ssoLogin.jsp"
     driver.get(URL)
-
-    id = ID
-    passwd = PW
-
     print("로그인 시작")
+
     driver.find_element(By.XPATH, '//*[@id="usr_id"]').click()
-    time.sleep(1)
+    WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, '//*[@id="usr_id"]')))
+    
     driver.find_element(By.XPATH, '//*[@id="usr_id"]').send_keys(id)
-
     driver.find_element(By.XPATH, '//*[@id="usr_pw"]').click()
-    time.sleep(1)
+    WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, '//*[@id="usr_pw"]')))
+    
     driver.find_element(By.XPATH, '//*[@id="usr_pw"]').send_keys(passwd)
-
-    time.sleep(2)
+    WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, '//*[@id="idLoginForm"]/div[1]/div[3]/button')))
+    
     driver.find_element(By.XPATH, '//*[@id="idLoginForm"]/div[1]/div[3]/button').click()
-
-    time.sleep(10)
-
+    WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, '//*[@id="Mainframe.VFrameSet.TopFrame.form.mnTop.item1:text"]')))
+    
     driver.find_element(By.XPATH, '//*[@id="Mainframe.VFrameSet.TopFrame.form.mnTop.item1:text"]').click()
-    time.sleep(2)
+    WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, '//*[@id="Mainframe.VFrameSet.HFrameSet.LeftFrame.form.tabMenu.tabMnu.form.grdMnLeft.body.gridrow_2.cell_2_0.celltreeitem.treeitemtext:text"]')))
+
 
     driver.find_element(By.XPATH, '//*[@id="Mainframe.VFrameSet.HFrameSet.LeftFrame.form.tabMenu.tabMnu.form.grdMnLeft.body.gridrow_2.cell_2_0.celltreeitem.treeitemtext:text"]').click()
-    time.sleep(2)
-    driver.find_element(By.XPATH, '//*[@id="Mainframe.VFrameSet.HFrameSet.innerVFrameSet.innerHFrameSet.innerVFrameSet2.WorkFrame.0001300.form.rdHakjum.radioitem1:icontext"]/img').click()
-    time.sleep(2)
+    WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, '//*[@id="Mainframe.VFrameSet.HFrameSet.innerVFrameSet.innerHFrameSet.innerVFrameSet2.WorkFrame.0001300.form.rdHakjum.radioitem1:icontext"]/img')))
+
+    driver.find_element(By.XPATH, '//*[@id="Mainframe.VFrameSet.HFrameSet.innerVFrameSet.innerHFrameSet.innerVFrameSet2.WorkFrame.0001300.form.rdHakjum.radioitem1:icontext"]/img').click()  
     print("로그인 종료")
 
     answer = []
@@ -58,17 +57,40 @@ def craw(ID, PW, year, semester):
             answer.append(grade)
             i = i + 1
         except NoSuchElementException:
-            flag = False
-            
+            flag = False      
     if year == "all":
         return answer
     else:
-        selection = year+"년도 "+semester + "학기 "
+        selection = str(year)+"년도 "+str(semester) + "학기"
         answer = filter_strings(answer,selection)
+        title = ""
+        mystr = ""
+        for item in answer:
+            #'2023년도 2학기 빅컨셉+ 90 A'로 되어있기에 분리함.
+            contents = item.split(" ")
+            if (len(title) <= 0):
+                title = contents[0] +" " + contents[1] + "의 성적을 안내드리겠습니다." 
+            
+            subject = contents[2] #과목
+            point = contents[3] #점수
+            grade = contents[4] #등급
+            if point == "10":
+                mystr += subject + last_string_check(subject) + " " + "패스등급을 받았습니다"
+            else :
+                mystr += subject + last_string_check(subject) + " " + point + "점을 맞았고" + grade + "의 등급을 받았습니다."
+        new_answer = title + mystr 
 
     print("크롤링 종료")
-    return answer
+    return new_answer
 
 def filter_strings(arr,selection):
     return [s for s in arr if f"{selection}" in s]
 
+
+def last_string_check(word):    #아스키(ASCII) 코드 공식에 따라 입력된 단어의 마지막 글자 받침 유무를 판단해서 뒤에 붙는 조사를 리턴하는 함수
+    last = word[-1]
+    criteria = (ord(last) - 44032) % 28
+    if criteria == 0:       #나머지가 0이면 받침이 없는 것
+        return '는'
+    else:                   #나머지가 0이 아니면 받침 있는 것
+        return '은'
